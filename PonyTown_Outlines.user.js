27ef84a84cc9b7a7567baa066b7a7b0fae3b5e05
Neeve <winwyv@gmail.com>
2017-12-18 18:30:44 +0300

    Update to 0.31.0pre1

--- PonyTown_Outlines.user.js
+++ PonyTown_Outlines.user.js
@@ -3,109 +3,21 @@
 // @namespace   azedith
 // @include     https://pony.town/*
 // @author      @NotMyWing
-// @version     0.29.0pre1
+// @version     0.31.0pre1
 // @grant       none
 // @downloadURL	https://github.com/Neeve01/PonyTown-Outlines/raw/master/PonyTown_Outlines.user.js
+// @require     https://raw.githubusercontent.com/Neeve01/PonyTown-Import-Export/master/ponytown_utils.js
 // ==/UserScript==
 
 (function(window, document) {
     'use strict';
 
     var rgb2hex = (r, g, b) => ((1 << 24 | Math.ceil(r) << 16 | Math.ceil(g) << 8 | Math.ceil(b)) >>> 0).toString(16).substr(1);
-
-    var clickEvent = new Event('click');
-    var inputEvent = new Event('input');
-    var mousedownEvent = new Event('mousedown');
-    var mouseupEvent = new Event('mouseup');
-
-    var Utils = {
-        IsCheckboxChecked: function(element) {
-            return element.children[0].children.length > 0;
-        },
-        GetFillOutlineValues: function(element) {
-            if (element.tagName == "FILL-OUTLINE") {
-                let divs = element.querySelectorAll("fill-outline > div > div");
-                let color = null,
-                    outline = null;
-
-                if (divs[0] && divs[1]) {
-                    let checkbox = divs[0].querySelector("check-box");
-                    if (!checkbox || (checkbox && !this.IsCheckboxChecked(checkbox))) {
-                        let picker = divs[1].querySelector("color-picker");
-                        color = this.GetTextValue(picker);
-                    }
-                }
-
-                if (divs[2] && divs[3]) {
-                    let checkbox = divs[2].querySelector("check-box");
-                    if (!checkbox || (checkbox && !this.IsCheckboxChecked(checkbox))) {
-                        let picker = divs[3].querySelector("color-picker");
-                        outline = this.GetTextValue(picker);
-                    }
-                }
-
-                return [color, outline]
-            }
-        },
-        SetFillOutlineValues: function(element, value) {
-            if (element.tagName == "FILL-OUTLINE") {
-                let color = value[0] || null;
-                let outline = value[1] || null;
-
-                let divs = element.querySelectorAll("fill-outline > div > div");
-
-                if (divs[0] && divs[1]) {
-                    let checkbox = divs[0].querySelector("check-box");
-
-                    let should_change = true;
-                    if (checkbox) {
-                        should_change = !!color;
-                        this.SetCheckbox(checkbox, !should_change);
-                    }
-
-                    if (should_change) {
-                        let picker = divs[1].querySelector("color-picker");
-                        this.SetTextValue(picker, color || "FFFFFF");
-                    }
-                }
-
-                if (divs[2] && divs[3]) {
-                    let checkbox = divs[2].querySelector("check-box");
-
-                    let should_change = true;
-                    if (checkbox) {
-                        should_change = !!outline;
-                        this.SetCheckbox(checkbox, !should_change);
-                    }
-
-                    if (should_change) {
-                        let picker = divs[3].querySelector("color-picker");
-                        this.SetTextValue(picker, outline || "000000");
-                    }
-                }
-            }
-        },
-        SetCheckbox: function(element, value) {
-            if (element.tagName == "CHECK-BOX") {
-                if (this.IsCheckboxChecked(element) != value) {
-                    element.children[0].dispatchEvent(clickEvent);
-                }
-            }
-        },
-        GetTextValue: function(element) {
-            if (element.tagName == "COLOR-PICKER") {
-                return element.querySelector("input").value;
-            }
-            return element.value;
-        },
-        SetTextValue: function(element, value) {
-            if (element.tagName == "COLOR-PICKER") {
-                element = element.querySelector("input");
-            }
-            element.value = value;
-            element.dispatchEvent(inputEvent);
-        },
+    var svg = {
+        link: '<svg version="1.1" class="nmw-color-picker-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 465.951 465.951"><path d="M441.962,284.364l-59.389-59.383c-15.984-15.985-35.396-23.982-58.238-23.982c-23.223,0-43.013,8.375-59.385,25.125l-25.125-25.125c16.751-16.368,25.125-36.256,25.125-59.671c0-22.841-7.898-42.157-23.698-57.958l-58.815-59.097c-15.798-16.178-35.212-24.27-58.242-24.27c-22.841,0-42.16,7.902-57.958,23.7L24.267,65.386C8.088,81.188,0,100.504,0,123.343c0,22.841,7.996,42.258,23.982,58.245l59.385,59.383c15.99,15.988,35.404,23.982,58.245,23.982c23.219,0,43.015-8.374,59.383-25.126l25.125,25.126c-16.75,16.371-25.125,36.258-25.125,59.672c0,22.843,7.898,42.154,23.697,57.958l58.82,59.094c15.801,16.177,35.208,24.27,58.238,24.27c22.844,0,42.154-7.897,57.958-23.698l41.973-41.682c16.177-15.804,24.271-35.118,24.271-57.958C465.947,319.771,457.953,300.359,441.962,284.364z M200.999,162.178c-0.571-0.571-2.334-2.378-5.28-5.424c-2.948-3.046-4.995-5.092-6.136-6.14c-1.143-1.047-2.952-2.474-5.426-4.286c-2.478-1.809-4.902-3.044-7.28-3.711c-2.38-0.666-4.998-0.998-7.854-0.998c-7.611,0-14.084,2.666-19.414,7.993c-5.33,5.327-7.992,11.799-7.992,19.414c0,2.853,0.332,5.471,0.998,7.851c0.666,2.382,1.903,4.808,3.711,7.281c1.809,2.474,3.237,4.283,4.283,5.426c1.044,1.141,3.09,3.188,6.136,6.139c3.046,2.95,4.853,4.709,5.424,5.281c-5.711,5.898-12.563,8.848-20.555,8.848c-7.804,0-14.277-2.568-19.414-7.705L62.81,142.761c-5.327-5.33-7.992-11.802-7.992-19.417c0-7.421,2.662-13.796,7.992-19.126l41.971-41.687c5.523-5.14,11.991-7.705,19.417-7.705c7.611,0,14.083,2.663,19.414,7.993l58.813,59.097c5.33,5.33,7.992,11.801,7.992,19.414C210.418,149.321,207.278,156.27,200.999,162.178z M403.147,361.732l-41.973,41.686c-5.332,4.945-11.8,7.423-19.418,7.423c-7.809,0-14.27-2.566-19.41-7.707l-58.813-59.101c-5.331-5.332-7.99-11.8-7.99-19.41c0-7.994,3.138-14.941,9.421-20.841c0.575,0.567,2.334,2.381,5.284,5.42c2.95,3.046,4.996,5.093,6.14,6.14c1.143,1.051,2.949,2.478,5.42,4.288c2.478,1.811,4.9,3.049,7.282,3.713c2.382,0.667,4.997,0.999,7.851,0.999c7.618,0,14.086-2.665,19.418-7.994c5.324-5.328,7.994-11.8,7.994-19.41c0-2.854-0.339-5.472-1-7.851c-0.67-2.382-1.902-4.809-3.72-7.282c-1.811-2.471-3.23-4.284-4.281-5.428c-1.047-1.136-3.094-3.183-6.139-6.14c-3.046-2.949-4.853-4.709-5.428-5.276c5.715-6.092,12.566-9.138,20.554-9.138c7.617,0,14.085,2.663,19.41,7.994l59.388,59.382c5.332,5.332,7.995,11.807,7.995,19.417C411.132,350.032,408.469,356.415,403.147,361.732z"/></svg>',
+        unlink: '<svg version="1.1" class="nmw-color-picker-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 475.082 475.082"><path d="M107.067,317.195c1.713-1.708,2.568-3.898,2.568-6.563c0-2.663-0.855-4.853-2.568-6.571c-1.714-1.707-3.905-2.562-6.567-2.562H9.135c-2.666,0-4.853,0.855-6.567,2.562C0.859,305.772,0,307.962,0,310.632c0,2.665,0.855,4.855,2.568,6.563c1.714,1.711,3.905,2.566,6.567,2.566H100.5C103.166,319.766,105.353,318.91,107.067,317.195z"/><path d="M310.629,109.634c2.669,0,4.859-0.855,6.563-2.568c1.718-1.711,2.574-3.901,2.574-6.567V9.138c0-2.659-0.856-4.85-2.574-6.565c-1.704-1.711-3.895-2.57-6.563-2.57c-2.662,0-4.853,0.859-6.563,2.57c-1.711,1.713-2.566,3.903-2.566,6.565v91.361c0,2.666,0.855,4.856,2.566,6.567C305.784,108.779,307.974,109.634,310.629,109.634z"/><path d="M118.771,347.184c-2.478,0-4.664,0.855-6.567,2.563l-73.089,73.087c-1.713,1.902-2.568,4.093-2.568,6.567c0,2.474,0.855,4.664,2.568,6.566c2.096,1.708,4.283,2.57,6.567,2.57c2.475,0,4.665-0.862,6.567-2.57l73.089-73.087c1.714-1.902,2.568-4.093,2.568-6.57c0-2.471-0.854-4.661-2.568-6.563C123.436,348.039,121.245,347.184,118.771,347.184z"/><path d="M356.315,127.905c2.283,0,4.473-0.855,6.571-2.565l73.087-73.089c1.707-1.903,2.562-4.093,2.562-6.567c0-2.475-0.855-4.665-2.562-6.567c-1.91-1.709-4.093-2.568-6.571-2.568c-2.471,0-4.66,0.859-6.563,2.568l-73.087,73.089c-1.708,1.903-2.57,4.093-2.57,6.567c0,2.474,0.862,4.661,2.57,6.567C351.846,127.05,354.037,127.905,356.315,127.905z"/><path d="M350.607,193.005c-4-3.999-9.328-7.994-15.988-11.991l-5.14,68.238l78.23,78.508c5.328,5.328,7.987,11.807,7.987,19.417c0,7.423-2.662,13.802-7.987,19.13l-41.977,41.686c-5.146,5.146-11.608,7.666-19.417,7.566c-7.81-0.1-14.271-2.707-19.411-7.854l-77.946-78.225l-68.234,5.144c3.999,6.656,7.993,11.988,11.991,15.985l95.362,95.643c15.803,16.18,35.207,24.27,58.238,24.27c22.846,0,42.154-7.898,57.957-23.695l41.977-41.685c16.173-15.8,24.27-35.115,24.27-57.958c0-22.46-7.994-41.877-23.982-58.248L350.607,193.005z"/><path d="M472.518,157.889c-1.711-1.709-3.901-2.565-6.563-2.565h-91.365c-2.662,0-4.853,0.855-6.563,2.565c-1.715,1.713-2.57,3.903-2.57,6.567c0,2.666,0.855,4.856,2.57,6.567c1.711,1.712,3.901,2.568,6.563,2.568h91.365c2.662,0,4.853-0.856,6.563-2.568c1.708-1.711,2.563-3.901,2.563-6.567C475.082,161.792,474.226,159.602,472.518,157.889z"/><path d="M109.348,67.099c5.523-5.14,11.991-7.705,19.417-7.705c7.611,0,14.084,2.663,19.414,7.993l77.943,78.227l68.234-5.142c-4-6.661-7.99-11.991-11.991-15.987l-95.358-95.643c-15.798-16.178-35.212-24.27-58.242-24.27c-22.841,0-42.16,7.902-57.958,23.7L28.837,69.955C12.659,85.756,4.57,105.073,4.57,127.912c0,22.463,7.996,41.877,23.982,58.245l95.93,95.93c3.995,4.001,9.325,7.995,15.986,11.991l5.139-68.521L67.377,147.33c-5.327-5.33-7.992-11.801-7.992-19.417c0-7.421,2.662-13.796,7.992-19.126L109.348,67.099z"/><path d="M164.454,365.451c-2.667,0-4.854,0.855-6.567,2.563c-1.711,1.711-2.568,3.901-2.568,6.57v91.358c0,2.669,0.854,4.853,2.568,6.57c1.713,1.707,3.9,2.566,6.567,2.566c2.666,0,4.853-0.859,6.567-2.566c1.713-1.718,2.568-3.901,2.568-6.57v-91.358c0-2.662-0.855-4.853-2.568-6.57C169.306,366.307,167.116,365.451,164.454,365.451z"/></svg>'
     }
+
 
     let Injection = {
         Style: `
@@ -124,10 +36,32 @@
             padding: 7px 0;
             text-align: center;
         }
+
+        .nmw-color-picker-svg {
+            width: 0.95em;
+        }
         `,
         CSS_RGB_Regex: /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/,
         InjectedCSSTag: null,
         ChevronStyle: null,
+        SetChain: function(e, state) {
+            if (!e.querySelector("svg")) {
+                e.setAttribute("state", !state);
+            }
+
+            let e_state = e.getAttribute("state")
+            if (e_state != state.toString()) {
+                e.setAttribute("state", state);
+                let s = e.querySelector("svg")
+                if (s)
+                    s.remove()
+
+                if (state)
+                    e.innerHTML = svg.link;
+                else
+                    e.innerHTML = svg.unlink;
+            }
+        },
         Inject: function(fill_outline) {
             let injection = this;
 
@@ -178,12 +112,8 @@
 
                 // Add a chain button.
                 let chain = document.createElement("div");
-                let i = document.createElement("i");
-                i.classList.add("fa")
-                i.classList.add("fa-fw")
-                i.classList.add("fa-chain")
-                chain.appendChild(i);
                 chain.classList.add("nmw-color-picker-chain");
+                this.SetChain(chain, true);
 
                 container.prepend(chain);
 
@@ -230,28 +160,22 @@
                     let [current_main, current_outline, outline_mimic_colors, shade_mimic_colors] = get_colors();
 
                     if (outline_mimic_colors.r <= 255 && outline_mimic_colors.g <= 255 && outline_mimic_colors.b <= 255) {
-                        i.style.color = "#888";
+                        chain.style.fill = "#AAA";
                     } else if (shade_mimic_colors.r <= 255 && shade_mimic_colors.g <= 255 && shade_mimic_colors.b <= 255) {
-                        i.style.color = "#666";
+                        chain.style.fill = "#666";
                     } else {
-                        i.style.color = "#444";
-                        if (i.classList.contains("fa-chain")) {
-                            i.classList.remove("fa-chain");
-                            i.classList.add("fa-chain-broken");
-                        }
+                        chain.style.fill = "#444";
+                        injection.SetChain(chain, false);
                         return;
                     }
-                    if (i.classList.contains("fa-chain-broken")) {
-                        i.classList.remove("fa-chain-broken");
-                        i.classList.add("fa-chain");
-                    }
+                    injection.SetChain(chain, true);
                 }
 
                 color_pickers[0].querySelector(".color-picker-box").addEventListener("color-change", main_watch, true);
 
                 // Add on-click and such.
                 chain.onclick = function() {
-                    let [main, outline] = Utils.GetFillOutlineValues(fill_outline);
+                    let [main, outline] = PonyTownUtils.GetFillOutlineValues(fill_outline);
 
                     if (main && outline) {
                         let [current_main, current_outline, outline_mimic_colors, shade_mimic_colors] = get_colors();
@@ -269,7 +193,7 @@
                         } else {
                             return;
                         }
-                        Utils.SetFillOutlineValues(fill_outline, [main, outline]);
+                        PonyTownUtils.SetFillOutlineValues(fill_outline, [main, outline]);
                     }
                 }
 
